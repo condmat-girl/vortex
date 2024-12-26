00e9f33d442abf59fdc60724481498c69fd1b516
condmat-girl <yelizaveta.kulynych@epfl.ch>
2024-12-27 00:35:14 +0100

    just clean the code, left only last working version

--- test.py
+++ test.py
@@ -1,35 +1,127 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from vortex import Vortex
+
+from vortex import Vortex,eps
 
 
-##test for energy density
 
-# vortex = Vortex(n=-1, R=20)
+### 1
+
+# vortex=Vortex(n=1,R=20)
 # vortex.plot()
-# vortex.plot_energy_density()
+
+
+# vortex=Vortex(n=1,R=20)
+# vortex.plot()
+
+# vortex=Vortex(n=0,R=20)
+# vortex.plot()
+
+# vortex=Vortex(n=-1,R=20)
+# vortex.plot()
+
+### 2
+
+winding = np.arange(-1, 2)  
+R_values = np.linspace(10, 60, 50)  
 
 
 
-##test for behaviour of energies with radius
+# energies_xy = np.zeros((len(winding),len(R_values)))
+# energies_dd = np.zeros((len(winding),len(R_values)))
+
+# for i, n in enumerate(winding):
+#     for j, R in enumerate(R_values):
+#         vortex = Vortex(n=n,R=R)
+#         energies_xy[i,j] = vortex.E_xy
+#         energies_dd[i,j] = vortex.E_dd
+#         print(len(winding)-i, len(R_values)-j)
 
 
-radii = np.arange(10, 60)  
-E_xy_values = []
-E_dd_values = []
-
-for R in radii:
-    vortex = Vortex(n=1, R=R)  
-    E_xy_values.append(vortex.E_xy) 
-    E_dd_values.append(vortex.E_dd)  
+# np.savetxt('xy_file',energies_xy)
+# np.savetxt('dd_file',energies_dd)
 
 
 
-plt.figure(figsize=(10, 6))
-plt.plot(radii, E_xy_values, label=r"$E_{xy}$ ", marker="o", color="blue")
-plt.plot(radii, E_dd_values, label=r"$E_{dd}$ ", marker="s", color="red")
-plt.xlabel("Radius (R)")
-plt.ylabel("Energy")
-plt.legend()
-plt.grid()
+### 3
+
+# J = 0.1
+
+# energies_xy = np.loadtxt('xy_file')
+# energies_dd = np.loadtxt('dd_file')
+
+# # energies = np.zeros((len(winding),len(R_values)))
+
+
+# # for i in range()
+# #     # energies[i,:] = energies_xy[i,:] + J* energies_dd[i,:]
+# #     plt.plot(R_values,energies[i,:], label = 'n = ' + str(windingn))
+
+# for i, n in enumerate(winding):
+#     plt.plot(R_values,energies_xy[i,:]+1e-3*i, label = 'n = ' + str(n))
+# plt.legend()
+# plt.ylabel("$E_{xy}$ [$J_1$]")
+# plt.xlabel("R [$R_0$]")
+# plt.savefig('xy_energies')
+# plt.show()
+
+# for i, n in enumerate(winding):
+#     plt.plot(R_values,energies_dd[i,:], label = 'n = ' + str(n))
+# plt.legend()
+# plt.ylabel("$E_{dd}$ [$J_2$]")
+# plt.xlabel("R [$R_0$]")
+# plt.savefig('dd_energies')
+# plt.show()
+    
+
+
+
+#####4
+
+# R = 5
+
+# x = np.arange(-R - 1, R) + 0.5
+# x, y = np.meshgrid(x, x)
+# print(x)
+
+# print(y)
+
+
+######5
+
+# for i in [-1,0,1]:
+#     vortex = Vortex(n=i, R=20)
+#     vortex.plot_density()
+# plt.show()
+
+
+#####6
+
+R = 20
+
+vortex1 = Vortex(n=1, R=R)
+vortex2 = Vortex(n=-1, R=R)
+
+
+
+
+
+plt.figure()
+
+im = plt.scatter(
+    vortex1.x,
+    vortex1.y,
+    c=vortex1.E_dd_density- vortex2.E_dd_density,
+    vmin=-2,
+    vmax=2,
+    cmap='coolwarm' 
+)
+cbar=plt.colorbar(im)
+
+
+plt.title("Vortex")
+plt.xlabel("x-axis")
+plt.ylabel("y-axis")
+plt.axis("equal")
+# plt.legend(loc="upper right")
 plt.show()
--- vortex.py
+++ vortex.py
@@ -2,13 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numba
 
-eps = 1e-5
+eps = 1e-8
+
 
 @numba.njit(fastmath=True)
 def calculate_dipole_dipole(theta, beta, d):
     N = len(theta)
-    E_density = np.zeros(N)
-    E_dd = 0
+
+    E_dd = np.zeros((N,N))
     for i in range(N):
         for j in range(N):
             if i != j:
@@ -17,23 +18,22 @@ def calculate_dipole_dipole(theta, beta, d):
                 term_2 = (
                     3 * np.cos(theta[i] - beta[i, j]) * np.cos(theta[j] - beta[i, j])
                 )
-                energy = prefact * (term_1 - term_2)
-                E_dd = energy
-                E_density[i] = energy
-    return E_dd, E_density
+                E_dd[i,j] += prefact * (term_1 + term_2)
+    return np.sum(E_dd), E_dd.sum(axis=1)
 
 
 class Vortex(object):
     def __init__(self, n=0, R=1) -> None:
+
         x = np.arange(-R - 1, R) + 0.5
         x, y = np.meshgrid(x, x)
 
         r = np.sqrt(x**2 + y**2)
         self.x, self.y = x[r <= R], y[r <= R]
-        phi = np.arctan2(self.y, self.x) + np.pi / 2
+        phi = np.arctan2(self.y, self.x) + np.pi/2
         self.u = np.cos(n * phi)
         self.v = np.sin(n * phi)
-        self.theta = np.arctan2(self.v, self.u)
+        self.theta = np.arctan2(self.v, self.u) 
 
         dx = self.x[:, None] - self.x[None, :]
         dy = self.y[:, None] - self.y[None, :]
@@ -43,19 +43,20 @@ class Vortex(object):
         self.beta = np.arctan2(dy, dx)
 
         i, j = self.nn
-        self.E_xy = np.sum(np.cos(self.theta[i] - self.theta[j]))
+        self.E_xy = - np.sum(np.cos(self.theta[i] - self.theta[j]))
 
         self.N = len(self.x)
 
-        self.E_dd, self.E_density = calculate_dipole_dipole(
-            theta=self.theta, beta=self.beta, d=self.d
-        )
+        self.E_dd, self.E_dd_density = calculate_dipole_dipole(theta=self.theta, beta=self.beta, d=self.d)
 
         self.E_dd /= self.N
         self.E_xy /= self.N
 
+
     def plot(self):
+
         plt.figure()
+
         plt.quiver(
             self.x,
             self.y,
@@ -66,33 +67,57 @@ class Vortex(object):
             pivot="mid",
             color="blue",
         )
+
         plt.title("Vortex")
         plt.xlabel("x-axis")
         plt.ylabel("y-axis")
         plt.axis("equal")
+        plt.legend(loc="upper right")
         plt.show()
 
-    # def plot_energy_density(self):
-    #     plt.figure()
-    #     plt.tricontourf(
-    #         self.x, self.y, self.E_density, cmap="viridis", levels=100
-    #     )
-    #     plt.colorbar(label="Energy Density")
-    #     plt.title("Energy Density of Dipole-Dipole Interaction")
-    #     plt.xlabel("x-axis")
-    #     plt.ylabel("y-axis")
-    #     plt.axis("equal")
-    #     plt.show()
 
 
-    def plot_energy_density(self):
-        plt.figure(figsize=(8, 6))
-        scatter = plt.scatter(
-            self.x, self.y, c=self.E_density, cmap="coolwarm"#, s=500
+    def plot_density(self):
+
+        plt.figure()
+
+        im = plt.quiver(
+            self.x,
+            self.y,
+            self.u,
+            self.v,
+            self.E_dd_density,
+            scale=0.8,
+            scale_units="xy",
+            pivot="mid",
+
+            
         )
-        plt.colorbar(scatter, label="Energy Density")
-        plt.title("Energy Density")
+        cbar=plt.colorbar(im)
+
+
+        plt.title("Vortex")
         plt.xlabel("x-axis")
         plt.ylabel("y-axis")
         plt.axis("equal")
+        plt.legend(loc="upper right")
+        plt.show()
+
+
+
+
+
+    
+    def plot_energy_density(self):
+        plt.figure(figsize=(8, 6))
+        plt.imshow(
+            self.E_dd_density,
+            extent=[self.x.min(), self.x.max(), self.y.min(), self.y.max()],
+            origin="lower",
+            cmap="viridis",
+        )
+        plt.colorbar(label="Dipole-Dipole Energy Density")
+        plt.title("Energy Density on Lattice")
+        plt.xlabel("x-axis")
+        plt.ylabel("y-axis")
         plt.show()
